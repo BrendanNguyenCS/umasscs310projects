c382f9dbdf9770662b96af03b6cc17f289e56ba6
Brendan Nguyen <bnguyen.andover@gmail.com>
2022-09-20 15:02:34 -0400

    adding hw1 basic code solutions

---
+++ homework/hw1/src/CombinationLock.java
@@ -0,0 +1,30 @@
+public class CombinationLock {
+    private int combination; // The lock's combination
+    private boolean isOpen; // The lock's lock status
+
+    // Constructor
+    public CombinationLock(int combination) {
+        this.combination = combination;
+        this.isOpen = false;
+    }
+
+    // Opens the lock if the attempted combination matches the lock's combination
+    public void open(int attempt) {
+        if (attempt == combination) {
+            isOpen = true;
+        } else {
+            System.out.printf("The combination %d is incorrect.\n", attempt);
+        }
+    }
+
+    // Locks the combination lock
+    public void lock() {
+        this.isOpen = false;
+    }
+
+    // Changes the lock's combination
+    public void changeCombo(int newCombination) {
+        this.combination = newCombination;
+        this.isOpen = false;
+    }
+}
---
+++ homework/hw1/src/WordUsage.java
@@ -0,0 +1,35 @@
+public class WordUsage {
+    private final String word; // the current word being tracked
+    private int count; // the number of times this word has been used
+
+    // Constructors
+    public WordUsage(String word, int count) {
+        this.word = word;
+        this.count = count;
+    }
+
+    public WordUsage(String word) {
+        this.word = word;
+        this.count = 1;
+    }
+
+    // Returns this tracked word
+    public String getWord() {
+        return this.word;
+    }
+
+    // Returns this word's count
+    public int getCount() {
+        return this.count;
+    }
+
+    // Sets this word's count
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    // Increments the count for this word
+    public void increment() {
+        this.count++;
+    }
+}
