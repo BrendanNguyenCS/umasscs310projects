a6345f9fdc402a0b3af03e5dd2a1d5cc9a17b194
Brendan Nguyen <bnguyen.andover@gmail.com>
2022-10-16 14:12:03 -0400

    pa1 initial commit

--- homework/hw1/src/CombinationLock.java
+++ homework/hw1/src/CombinationLock.java
@@ -1,32 +1,28 @@
 // Homework 1 Question 8
 
 public class CombinationLock {
-    private int combination; // The lock's combination
-    private boolean isOpen; // The lock's lock status
+    private int a, b, c; // The lock's combination
 
     // Constructor
-    public CombinationLock(int combination) {
-        this.combination = combination;
-        this.isOpen = false;
+    public CombinationLock(int a1, int a2, int a3) {
+        this.a = a1;
+        this.b = a2;
+        this.c = a3;
     }
 
     // Opens the lock if the attempted combination matches the lock's combination
-    public void open(int attempt) {
-        if (attempt == combination) {
-            isOpen = true;
-        } else {
-            System.out.printf("The combination %d is incorrect.\n", attempt);
+    public boolean open(int x, int y, int z) {
+        return x == a && y == b && z == c;
+    }
+
+    // Changes the lock's combination (old combination followed by new combination)
+    public boolean changeCombo(int x, int y, int z, int p, int q, int r) {
+        if (this.open(x, y, z)) {
+            a = p;
+            b = q;
+            c = r;
+            return true;
         }
-    }
-
-    // Locks the combination lock
-    public void lock() {
-        this.isOpen = false;
-    }
-
-    // Changes the lock's combination
-    public void changeCombo(int newCombination) {
-        this.combination = newCombination;
-        this.isOpen = false;
+        return false;
     }
 }
