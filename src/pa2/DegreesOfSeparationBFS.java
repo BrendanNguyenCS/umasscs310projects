14c37fbe7aca46ee2c84126df1d911462ac91cc8
Brendan Nguyen <bnguyen.andover@gmail.com>
2022-11-12 17:05:31 -0500

    Complete pa2

---
+++ src/pa2/DegreesOfSeparationBFS.java
@@ -0,0 +1,165 @@
+package pa2;
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.Stack;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.Graph;
+import edu.princeton.cs.algs4.SymbolGraph;
+import edu.princeton.cs.algs4.BreadthFirstPaths;
+
+/**
+ *  The {@code DegreesOfSeparation} class provides a client for finding
+ *  the degree of separation between one distinguished individual and
+ *  every other individual in a social network.
+ *  As an example, if the social network consists of actors in which
+ *  two actors are connected by a link if they appeared in the same movie,
+ *  and Kevin Bacon is the distinguished individual, then the client
+ *  computes the Kevin Bacon number of every actor in the network.
+ *  <p>
+ *  The running time is proportional to the number of individuals and
+ *  connections in the network. If the connections are given implicitly,
+ *  as in the movie network example (where every two actors are connected
+ *  if they appear in the same movie), the efficiency of the algorithm
+ *  is improved by allowing both movie and actor vertices and connecting
+ *  each movie to all of the actors that appear in that movie.
+ *  <p>
+ *  For additional documentation,
+ *  see <a href="https://algs4.cs.princeton.edu/41graph">Section 4.1</a> of
+ *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+ *
+ *  @author Robert Sedgewick
+ *  @author Kevin Wayne
+ */
+public class DegreesOfSeparationBFS {
+    private SymbolGraph sg;
+    private BreadthFirstPaths bfs;
+
+    // this class cannot be instantiated
+    public DegreesOfSeparationBFS(String fname, String delimiter, String source) {
+        sg = new SymbolGraph(fname, delimiter);
+        int sourceIndex = sg.indexOf(source);
+        bfs = new BreadthFirstPaths(sg.graph(), sourceIndex);
+    }
+
+    // Getters
+    public SymbolGraph getSymbolGraph() { return sg; }
+    public BreadthFirstPaths getBreadthFirstPaths() { return bfs; }
+
+    // Read actor's name from standard input, print bacon relationship
+    public int baconNumber(String sink)
+    {
+        // if the actor isn't listed
+        if (!sg.contains(sink)) {
+            return -1;
+        }
+        int sinkIndex = sg.indexOf(sink);
+        // no connection between the source and sink
+        if (!bfs.hasPathTo(sinkIndex)) {
+            return -1;
+        }
+        return bfs.distTo(sinkIndex)/2;
+    }
+
+    // Calculate the path itself.
+    public Stack<Integer> graphPath(String sink){
+        Stack<Integer> path = new Stack<Integer>();
+        // Get the sink index
+        int sinkIndex = sg.indexOf(sink);
+        // Add each part of the path between the actors to path stack
+        for (int g : bfs.pathTo(sinkIndex)) {
+            path.push(g);
+        }
+        return path;
+    }
+
+    public void printPath(Stack<Integer> path){
+        // Now print. Every other vertex is an actor
+        // Printing the current actor's bacon number
+        String actor = sg.nameOf(path.pop());
+        System.out.println(actor + " has a Bacon number of " + baconNumber(actor));
+        // Print out the path that connects the actor to Bacon
+        while (!path.isEmpty()) {
+            // Get movie title
+            String movie = sg.nameOf(path.pop());
+            // Get common actor in movie
+            String actor2 = sg.nameOf(path.pop());
+            System.out.println(actor + " was in \"" + movie + "\" with " + actor2);
+            // Update connecting actor in order to trace next connection
+            actor = actor2;
+        }
+    }
+
+    public void printHistogram() {
+        // compute histogram of Kevin Bacon numbers - 100 for infinity
+        int MAX_BACON = 100;
+        int[] hist = new int[MAX_BACON + 1];
+        Graph G = sg.graph();
+        for (int v = 0; v < G.V(); v++) {
+            int bacon = Math.min(MAX_BACON, bfs.distTo(v));
+            hist[bacon]++;
+
+            // to print actors and movies with large bacon numbers
+            if (bacon/2 >= 7 && bacon < MAX_BACON)
+                StdOut.printf("%d %s\n", bacon/2, sg.nameOf(v));
+        }
+
+        // print out histogram - even indices are actors
+        for (int i = 0; i < MAX_BACON; i += 2) {
+            if (hist[i] == 0) break;
+            StdOut.printf("%3d %8d\n", i/2, hist[i]);
+        }
+        StdOut.printf("Inf %8d\n", hist[MAX_BACON]);
+    }
+
+
+    /*
+     * @param args the command-line arguments
+     */
+    public static void main(String[] args) {
+        String filename  = args[0];
+        String delimiter = args[1];
+        String source    = args[2];
+
+        DegreesOfSeparationBFS baconGraph = new DegreesOfSeparationBFS(filename, delimiter, source);
+
+        baconGraph.printHistogram();
+
+        // Print the Bacon diagram
+        //baconGraph.printBaconDiagram();
+        /*int i, no_args = args.length;
+        // Get degrees of separation
+        for(i=3;i<no_args;i++) {
+            baconGraph.baconNumber(args[i]);
+            Stack<Integer> path = baconGraph.graphPath(args[i]);
+            baconGraph.printPath(path);
+        }*/
+    }
+}
+
+/******************************************************************************
+ *  Copyright 2002-2016, Robert Sedgewick and Kevin Wayne.
+ *
+ *  This file is part of algs4.jar, which accompanies the textbook
+ *
+ *      Algorithms, 4th edition by Robert Sedgewick and Kevin Wayne,
+ *      Addison-Wesley Professional, 2011, ISBN 0-321-57351-X.
+ *      http://algs4.cs.princeton.edu
+ *
+ *
+ *  algs4.jar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  algs4.jar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with algs4.jar.  If not, see http://www.gnu.org/licenses.
+ ******************************************************************************/
+
+
+
---
+++ src/pa2/Grid.java
@@ -0,0 +1,265 @@
+package pa2;
+
+import java.util.*;
+
+/**
+ * Class to demonstrate greedy algorithms. Skeleton.
+ */
+public class Grid {
+    private boolean[][] grid = null;
+    private ArrayList<Set<Spot>> allGroups; // All groups
+    private final int height; // The height of the grid
+    private final int width; // The width of the grid
+
+    /**
+     * Very simple constructor
+     *
+     * @param ingrid
+     * a two-dimensional array of boolean to be used as the grid to
+     * search
+     */
+    public Grid(boolean[][] ingrid) {
+        grid = ingrid;
+        allGroups = new ArrayList<Set<Spot> >();
+        height = grid.length;
+        width = grid[0].length;
+    }
+
+    /**
+     * Main method, creates a Grid, then asks it for the size of the group
+     * containing the given point.
+     */
+    public static void main(String[] args) {
+        // Check arguments here.
+
+        // Usage: java Grid 3 7 to search from (3, 7), top occupied square
+        // of L-shaped group.
+        // -all to print all sets.
+
+        boolean[][] gridData = {
+                { false, false, false, false, false, false, false, false,
+                        false, true },
+                { false, false, false, true, true, false, false, false, false,
+                        true },
+                { false, false, false, false, false, false, false, false,
+                        false, false },
+                { false, false, false, false, true, false, false, true, false,
+                        false },
+                { false, false, false, true, false, false, false, true, false,
+                        false },
+                { false, false, false, false, false, false, false, true, true,
+                        false },
+                { false, false, false, false, true, true, false, false, false,
+                        false },
+                { false, false, false, false, false, false, false, false,
+                        false, false },
+                { false, false, false, false, false, false, false, false,
+                        false, false },
+                { false, false, false, false, false, false, false, false,
+                        false, false } };
+        // Other stuff here.
+
+    }
+
+    public void printAllGroups()
+    {
+        for(Set<Spot> g:allGroups) {
+            for(Spot s:g)
+                System.out.println(s);
+            System.out.println("");
+        }
+    }
+    // The best way IMO to calculate the number of groups is to set up a matrix of integers and
+    // for each non-0 entry calculate the group it's in.
+    public ArrayList<Set<Spot>> calcAllGroups() {
+        HashSet<Spot> spotsTraversed = new HashSet<>(); // a set to keep track of spots already traversed
+
+        // Traverse grid
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                // if the current spot is not in a group or is already in a group
+                if (!grid[i][j] || spotsTraversed.contains(new Spot(i, j))) {
+                    // continue
+                }
+                else {
+                    // calculate group size
+                    int groupSize = groupSize(i, j);
+                    Set<Spot> currentGroup = new HashSet<Spot>();
+                    // Fill current group with all Spots in the group
+                    getGroup(i, j, groupSize, currentGroup, spotsTraversed);
+                    allGroups.add(currentGroup);
+                }
+            }
+        }
+        return allGroups;
+    }
+    /**
+     * Prints out a usage message
+     */
+    private static void printUsage() {
+        System.out.println("Usage: java Grid <i> <j>");
+        System.out.println("Find the size of the cluster of spots including position i,j");
+        System.out.println("Usage: java Grid -all");
+        System.out.println("Print all spots.");
+    }
+
+    /**
+     * This calls the recursive method to find the group size
+     *
+     * @param i
+     * the first index into grid (i.e. the row number)
+     * @param j
+     * the second index into grid (i.e. the col number)
+     * @return the size of the group
+     */
+    public int groupSize(int i, int j) {
+        HashSet<Spot> group = new HashSet<Spot>();
+        findGroup(i, j, group);
+        return group.size();
+    }
+
+    /**
+     * The recursive method that fills the set of the Spots in the group. This will be called by groupSize() to
+     * get the current group's size
+     *
+     * @param i
+     * the current row in the grid
+     * @param j
+     * the current column in the grid
+     * @param group
+     * the set that contains the current group
+     */
+    private void findGroup(int i, int j, Set<Spot> group) {
+        // Checking if i,j coordinates are out of bounds
+        if (!checkBounds(i, j)) {
+            return;
+        }
+
+        // Stop if current square is not in a group or is already in the current group
+        if (!grid[i][j] || group.contains(new Spot(i, j))) {
+            return;
+        }
+        else {
+            // Add current square to group
+            Spot s = new Spot(i, j);
+            group.add(s);
+            // Recursively search in all four directions
+            findGroup(i - 1, j, group); // Below
+            findGroup(i + 1, j, group); // Above
+            findGroup(i, j - 1, group); // Left
+            findGroup(i, j + 1, group); // Right
+        }
+    }
+
+    /**
+     * This private recursive method utilized by calcGroups() to populate the current group and the set that
+     * defines all the spots that have been visited. It also adds the known group size of the current group to all
+     * Spots
+     *
+     * @param i
+     * the current row position in the grid
+     * @param j
+     * the current column position in the grid
+     * @param count
+     * the current group's group size
+     * @param group
+     * the Set that defines the current group
+     * @param allSpots
+     * the Set that defines all Spots that are already in a group
+     */
+    private void getGroup(int i, int j, int count, Set<Spot> group, Set<Spot> allSpots) {
+        // Checking if i,j coordinates are out of bounds
+        if (!checkBounds(i, j)) {
+            return;
+        }
+
+        // Stop if current square is not in a group or is already in the current group
+        if (!grid[i][j] || group.contains(new Spot(i, j)) || allSpots.contains(new Spot(i, j))) {
+            return;
+        }
+        else {
+            // Add current square to group and allSpots
+            Spot s = new Spot(i, j);
+            s.setGroup(count);
+            group.add(s);
+            allSpots.add(s);
+            // Recursively add in all four directions
+            getGroup(i - 1, j, count, group, allSpots); // Below
+            getGroup(i + 1, j, count, group, allSpots); // Above
+            getGroup(i, j - 1, count, group, allSpots); // Left
+            getGroup(i, j + 1, count, group, allSpots); // Right
+        }
+    }
+
+    /**
+     * This method checks if the desired coordinates are within the bounds of the grid
+     *
+     * @param i
+     * the row coordinate
+     * @param j
+     * the column coordinate
+     * @return true if the coordinates are in bounds, false otherwise
+     */
+    private boolean checkBounds(int i, int j) {
+        return !(i < 0 || j < 0 || i >= height || j >= width);
+    }
+
+    /**
+     * Nested class to represent a filled spot in the grid
+     */
+    public static class Spot {
+        int i;
+        int j;
+        int group;
+        /**
+         * Constructor for a Spot
+         *
+         * @param i
+         * the i-coordinate of this Spot
+         * @param j
+         * the j-coordinate of this Spot
+         */
+        public Spot(int i, int j) {
+            this.i = i;
+            this.j = j;
+            this.group = 0; // Default. Will be set later.
+        }
+
+        /**
+         * Tests whether this Spot is equal (i.e. has the same coordinates) to
+         * another
+         *
+         * @param o
+         * an Object
+         * @return true if o is a Spot with the same coordinates
+         */
+        public boolean equals(Object o) {
+            if (o == null)
+                return false;
+            if (o.getClass() != getClass())
+                return false;
+            Spot other = (Spot) o;
+            return (other.i == i) && (other.j == j);
+        }
+
+        /**
+         * Returns an int based on Spot's contents
+         * another way: (new Integer(i)).hashCode()^(new Integer(j)).hashCode();
+         */
+        public int hashCode() {
+            return i << 16 + j; // combine i and j two halves of int
+        }
+
+        public void setGroup(int g) {group = g;}
+
+        public int getI() {return i;}
+        public int getJ() {return j;}
+        public int getGroup() {return group;}
+        /**
+         * Returns a String representing this Spot, just the coordinates. You can add group if you want.
+         */
+        public String toString() {
+            return "(" + i + " , " + j + ")";
+        }
+    }
+}
